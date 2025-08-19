c0b738f456242897e375ae0bd0ee5722f985e66a
Lieto-mieli <lieto.malmiola@outlook.com>
2025-08-19 09:15:08 +0300

    assignment 02 done

--- 2025 Syksy/Python/assignment02/MedievalMeasures.py
+++ 2025 Syksy/Python/assignment02/MedievalMeasures.py
@@ -1,6 +1,7 @@
-num0=int(input('Anna leiviskät.'))
-num1=int(input('Anna naulat.'))
-num2=int(input('Anna luodit.'))
-resultKilogrammat=int((g)/1000)
-resultGrammat=num0+num1+num2
+num0=float(input('Anna leiviskät.'))
+num1=float(input('Anna naulat.'))
+num2=float(input('Anna luodit.'))
+g=(num2+((num1+(num0*20))*32))*13.3
+resultKilogrammat=int(g/1000)
+resultGrammat=g-(resultKilogrammat*1000)
 print(f"{resultKilogrammat} kilogrammaa ja {resultGrammat} grammaa")
