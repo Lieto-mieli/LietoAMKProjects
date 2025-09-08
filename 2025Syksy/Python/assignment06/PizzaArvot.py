172c5a1e04748994590700c978dcf1fb74ab46cd
Lieto-mieli <lieto.malmiola@outlook.com>
2025-09-08 13:54:29 +0300

    pizzamathfix

--- 2025Syksy/Python/assignment06/PizzaArvot.py
+++ 2025Syksy/Python/assignment06/PizzaArvot.py
@@ -1,19 +1,20 @@
 import math
 def pizzaefficiency(halkaisija, hinta):
-    pintaala = (math.pi * pow(halkaisija, 2)) / 4
-    efficiency = pintaala / hinta
+    halkaisijameters = halkaisija / 100
+    pintaala = (math.pi * (pow(halkaisijameters, 2))) / 4
+    efficiency = hinta / pintaala
     return efficiency
-pizza1Halkaisija = input("Anna ensimmäisen pizza halkaisija: ")
-pizza1Hinta = input("Anna ensimmäisen pizza hinta: ")
-pizza2Halkaisija = input("Anna toisen pizza halkaisija: ")
-pizza2Hinta = input("Anna toisen pizza hinta: ")
+pizza1Halkaisija = float(input("Enter the diameter of the first pizza (cm): "))
+pizza1Hinta = float(input("Enter the price of the first pizza (euros): "))
+pizza2Halkaisija = float(input("Enter the diameter of the second pizza (cm): "))
+pizza2Hinta = float(input("Enter the price of the second pizza (euros): "))
 pizza1Efficiency = pizzaefficiency(pizza1Halkaisija, pizza1Hinta)
 pizza2Efficiency = pizzaefficiency(pizza2Halkaisija, pizza2Hinta)
 print(pizza1Efficiency)
 print(pizza2Efficiency)
-if pizza1Efficiency < pizza2Efficiency:
-    print("pizza 2 on edullisempi")
-elif pizza1Efficiency > pizza2Efficiency:
-    print("pizza 1 on edullisempi")
+if pizza1Efficiency > pizza2Efficiency:
+    print("The second pizza provides better value for money.")
+elif pizza1Efficiency < pizza2Efficiency:
+    print("The first pizza provides better value for money.")
 else:
     print("pizzat ovat yhtä edullisia")
